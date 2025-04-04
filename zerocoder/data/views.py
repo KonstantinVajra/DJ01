6165bbb6d5042d4906344401fbe2d41f83d7c44b
KonstantinVajra <kmkm27@mail.ru>
2025-04-04 04:55:31 +0300

    v1

---
+++ zerocoder/data/views.py
@@ -0,0 +1,9 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def data_text(request):
+    return HttpResponse("<h1>  Страница Data</h1>")
+
+
+# Create your views here.
