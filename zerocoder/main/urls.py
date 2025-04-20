24bd10859d1be671f15da6e249c7986a79fcf788
KonstantinVajra <kmkm27@mail.ru>
2025-04-20 21:30:15 +0300

    under del

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -1,9 +1,13 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('', views.index ),
-
-
-]
+    path('', views.home, name='home'),
+    path('new', views.new),
+    path('romeiro/', views.romeiro_view, name='romeiro'),
+    path('empty/', views.empty_island_view, name='empty_island'),
+    path('light/', views.light_path_view, name='light_path'),
+    path('sunset/', views.sunset_view, name='sunset'),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse("<h1> Это мой первый проект на Django!</h1>")
 
+def home(request):
+    return render(request, 'main/home.html')
+
+def new(request):
+    return render(request, 'main/new.html')
+
+def romeiro_view(request):
+    return render(request, 'main/romeiro.html')
+
+def empty_island_view(request):
+    return render(request, 'main/empty_island.html')
+
+def light_path_view(request):
+    return render(request, 'main/light_path.html')
+
+def sunset_view(request):
+    return render(request, 'main/sunset.html')
